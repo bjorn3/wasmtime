2059b56c145c975ff139e9ca096eee52d0f89bfe
bjorn3 <bjorn3@users.noreply.github.com>
2021-10-24 16:58:42 +0200

    Inline several functions

--- cranelift/codegen/meta/src/gen_types.rs
+++ cranelift/codegen/meta/src/gen_types.rs
@@ -20,21 +20,10 @@ fn emit_type(ty: &cdsl_types::ValueType, fmt: &mut srcgen::Formatter) {
     fmtln!(fmt, "pub const {}: Type = Type({:#x});\n", name, number);
 }
 
-/// Emit definition for all vector types with `bits` total size.
-fn emit_vectors(bits: u64, fmt: &mut srcgen::Formatter) {
-    let vec_size: u64 = bits / 8;
-    for vec in cdsl_types::ValueType::all_lane_types()
-        .map(|ty| (ty, cdsl_types::ValueType::from(ty).membytes()))
-        .filter(|&(_, lane_size)| lane_size != 0 && lane_size < vec_size)
-        .map(|(ty, lane_size)| (ty, vec_size / lane_size))
-        .map(|(ty, lanes)| cdsl_types::VectorType::new(ty, lanes))
-    {
-        emit_type(&cdsl_types::ValueType::from(vec), fmt);
-    }
-}
+/// Generate the types file.
+pub(crate) fn generate(filename: &str, out_dir: &str) -> Result<(), error::Error> {
+    let fmt = &mut srcgen::Formatter::new();
 
-/// Emit types using the given formatter object.
-fn emit_types(fmt: &mut srcgen::Formatter) {
     // Emit all of the special types, such as types for CPU flags.
     for spec in cdsl_types::ValueType::all_special_types().map(cdsl_types::ValueType::from) {
         emit_type(&spec, fmt);
@@ -52,14 +41,17 @@ fn emit_types(fmt: &mut srcgen::Formatter) {
 
     // Emit vector definitions for common SIMD sizes.
     for vec_size in &[64_u64, 128, 256, 512] {
-        emit_vectors(*vec_size, fmt);
+        let vec_size: u64 = vec_size / 8;
+        for vec in cdsl_types::ValueType::all_lane_types()
+            .map(|ty| (ty, cdsl_types::ValueType::from(ty).membytes()))
+            .filter(|&(_, lane_size)| lane_size != 0 && lane_size < vec_size)
+            .map(|(ty, lane_size)| (ty, vec_size / lane_size))
+            .map(|(ty, lanes)| cdsl_types::VectorType::new(ty, lanes))
+        {
+            emit_type(&cdsl_types::ValueType::from(vec), fmt);
+        }
     }
-}
 
-/// Generate the types file.
-pub(crate) fn generate(filename: &str, out_dir: &str) -> Result<(), error::Error> {
-    let mut fmt = srcgen::Formatter::new();
-    emit_types(&mut fmt);
     fmt.update_file(filename, out_dir)?;
     Ok(())
 }
